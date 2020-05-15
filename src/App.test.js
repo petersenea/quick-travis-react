39ec5568fb0a93616c73c91b0be68f8cd7988046
petersenea <Petersenea12@gmail.com>
2020-05-15 10:59:05 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/hello/i);
+  const linkElement = getByText(/hello3/i);
   expect(linkElement).toBeInTheDocument();
 });
